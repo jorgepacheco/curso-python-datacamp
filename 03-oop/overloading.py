f0cd1da32b626cc934579188a86cb046ae130bb6
Jorge Pacheco Mengual <jorge.pacheco12@gmail.com>
2026-02-20 19:32:14 +0100

    feat: :art: final class overloading method
    overloading

--- 03-oop/overloading.py
+++ 03-oop/overloading.py
@@ -32,3 +32,25 @@ pn = Phone(873555333)
 print(acct == pn)
 
 print(pn == acct)
+
+
+class Employee:
+    def __init__(self, name, salary=30000):
+        self.name, self.salary = name, salary
+
+    # Add the __repr__() method
+    def __repr__(self):
+        return f"Employee('{self.name}', {self.salary})"
+    # Add the __str__() method
+    def __str__(self):
+      emp_str = f"""Employee name: {self.name}
+Employee salary: {self.salary}"""
+      return emp_str
+
+emp1 = Employee("Amar Howard", 30000)
+print(repr(emp1))
+emp2 = Employee("Carolyn Ramirez", 35000)
+print(repr(emp2))
+
+print(emp1)
+print(emp2)
