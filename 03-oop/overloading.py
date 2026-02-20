646c6e6b70c49f0fa5410c530c792bb1d3f11808
jpacheco <jpacheco@techserv.ibercaja.es>
2026-02-20 18:14:49 +0100

    add overloading concept

---
+++ 03-oop/overloading.py
@@ -0,0 +1,34 @@
+
+class Phone:
+  def __init__(self, number):
+    self.number = number
+
+
+class BankAccount:
+  # Modify to initialize a number attribute
+  def __init__(self, number, balance=0):
+    self.balance = balance
+    self.number = number
+
+  def withdraw(self, amount):
+    self.balance -= amount
+
+  # Define __eq__ that returns True if the number attributes are equal
+  def __eq__(self, other):
+    return (self.number == other.number) and (type(self) == type(other))
+
+# Create accounts and compare them
+acct1 = BankAccount(123, 1000)
+acct2 = BankAccount(123, 1000)
+acct3 = BankAccount(456, 1000)
+print(acct1 == acct2)
+print(acct1 == acct3)
+
+
+acct = BankAccount(873555333)
+pn = Phone(873555333)
+
+# Check if the two objects are equal
+print(acct == pn)
+
+print(pn == acct)
